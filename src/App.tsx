be8a14f7c6500db2f9aa482224593288fd146f2f
dungtran <dungcq86@gmail.com>
2024-03-18 17:05:05 +0700

    base scss

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import { Footer, Header } from './components';
+import Home from './page/Home/Home';
+
+function App() {
+  return (
+    <div>
+    {/* Header */}
+    <Header />
+
+{/* Home */}
+<Home />
+
+    {/* Footer */}
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
